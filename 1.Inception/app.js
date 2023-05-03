d8c48012d688b90b6c9817e316e4936573e7c186
SunDeveloper1 <harikumar01947@gmail.com>
2023-05-03 23:38:52 +0530

    Ep-1 Incepstion

---
+++ 1.Inception/app.js
@@ -0,0 +1,25 @@
+/**
+ *^ AIM :- To create React App Using basic
+ */
+
+
+
+
+//! Creating a basic Element using React
+/**
+ * ~ React Element is a Javascript Object
+ */
+const element=React.createElement("h1",{
+    id:"heading",
+    xyz:"Vivek"
+},"Hello WOrld from Nasmte React🙏")
+
+console.log(element) //~ React Element 
+        
+        
+//! Creating a Root Element using ReactDOM
+const root=ReactDOM.createRoot(document.getElementById("root"))
+
+
+//! root element created by DOM will render every other element
+root.render(element)
